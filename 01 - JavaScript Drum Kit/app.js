5f9990b0ced80e2d2312d57784f5e4b414043f56
JoscelineO <josceline@mac.com>
2017-04-17 19:52:40 +0100

    audio on keydown

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,9 +1,21 @@
+window.onload = function() {
 
-window.addEventListener('keydown', function(e) {
-  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-  const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
-  if (!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-  key.classList.add('playing');
-});
+  window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+  });
+
+  function removeTransition(e) {
+    console.log('here');
+    console.log(e);
+  }
+
+  const keys = document.querySelectorAll('.key');
+  console.log(keys);
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+};
