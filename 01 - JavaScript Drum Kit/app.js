cc3a000ad35a1e213ce8c33b8a75531cba5db140
JoscelineO <josceline@mac.com>
2017-04-17 20:03:00 +0100

    added remove transition function

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -10,12 +10,11 @@ window.onload = function() {
   });
 
   function removeTransition(e) {
-    console.log('here');
-    console.log(e);
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
   }
 
   const keys = document.querySelectorAll('.key');
-  console.log(keys);
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 };
