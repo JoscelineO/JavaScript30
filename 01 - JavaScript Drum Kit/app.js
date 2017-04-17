fa45fa132aa7ea201d092e31fab02e317e5214d7
JoscelineO <josceline@mac.com>
2017-04-17 20:09:52 +0100

    created playSound function

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,13 +1,13 @@
 window.onload = function() {
 
-  window.addEventListener('keydown', function(e) {
+  function playSound(e) {
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
     const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
-  });
+  }
 
   function removeTransition(e) {
     if(e.propertyName !== 'transform') return;
@@ -17,4 +17,5 @@ window.onload = function() {
   const keys = document.querySelectorAll('.key');
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
+  window.addEventListener('keydown', playSound);
 };
