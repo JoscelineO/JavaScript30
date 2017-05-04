461e04772bd9ae5a4fbeedcec715ec255505c110
JoscelineO <josceline@mac.com>
2017-05-04 11:30:47 +0100

    minute hand working

--- 02 - JS and CSS Clock/app.js
+++ 02 - JS and CSS Clock/app.js
@@ -1,12 +1,21 @@
 window.onload = function() {
   const secondHand = document.querySelector('.second-hand');
+  const minsHand = document.querySelector('.min-hand');
+
   function setDate() {
     const now = new Date();
+
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds/60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds);
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes/60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    console.log(minutes);
   }
 
   setInterval(setDate, 1000);
+  setDate();
 };
